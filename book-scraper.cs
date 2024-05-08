c792038f66c0de67ae9834dea32533fbaf38043c
James Spalding <jamescodebase@outlook.com>
2024-05-08 21:46:35 +0100

    Added scraping packages and setup proj

---
+++ book-scraper.cs
@@ -0,0 +1,18 @@
+﻿using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace book_scraper
+{
+    public class book_scraper
+    {
+        static void scraper(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+
+    }
+}
